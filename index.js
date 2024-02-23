b493d2f4e4d67675318d8c9b09f5c0471e0c8ab7
Sahithy Tumma <200010054@iitdh.ac.in>
2024-02-23 12:05:16 +0000

    done

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+let bodyParser = require('body-parser');
+const multer = require('multer');
 
 var app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express.static('public'));
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +20,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  // Extract file information
+  const fileInfo = {
+    filename: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+
+  // Send file information in the response
+  res.json(fileInfo);
+});
 
 
 
